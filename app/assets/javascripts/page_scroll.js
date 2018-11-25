1a37c5cda6f7b6930eb40b181984c2e789b8fccc
ADRIAN NECHITA <nechitadi11@gmail.com>
2018-11-25 08:36:48 +0200

    Fix page scroll

--- app/assets/javascripts/page_scroll.js
+++ app/assets/javascripts/page_scroll.js
@@ -1,10 +1,9 @@
 $(document).on("turbolinks:load",function(){
   $("#main-navbar a").on('click', function(event) {
     if (this.hash !== "") {
-        event.preventDefault();
         var hash = this.hash;
         $('html, body').animate({
-            scrollTop: $(hash).offset().top
+            scrollTop: $(hash).offset().top - 56
             }, 369, function(){
             window.location.hash = hash;
         });
